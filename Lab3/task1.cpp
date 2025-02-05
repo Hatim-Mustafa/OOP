8c501abe0ad3606c2846987798228309c2510a3d
Hatim-Mustafa <hatimmustafa108@gmail.com>
2025-02-05 08:25:07 +0000

    Task 1 done

--- Lab3/task1.cpp
+++ Lab3/task1.cpp
@@ -1,1 +1,65 @@
+#include <iostream>
+using namespace std;
 
+//All new marker pens will have an ink level of MAX;
+const int MAX = 100;
+
+class MarkerPen {
+
+    public:
+        string brand;
+        string color;
+    private:
+        float inkLvl;
+        bool refillability;
+
+    public:
+        MarkerPen (string b, string c, bool r) {
+            brand = b;
+            color = c;
+            inkLvl = MAX;
+            refillability = r;
+        }
+
+        float showInkLvl () {return inkLvl;}
+
+        void write(int numOfWords) {
+            if (inkLvl > numOfWords) {
+                cout << numOfWords << " words have been written\n";
+                inkLvl -= numOfWords;
+            }
+            else {
+                cout << "Not Enough Ink\n";
+                refill();
+            }
+        }
+
+        bool isRefiilable () {return refillability;}
+
+        void refill () {
+            if (isRefiilable()) {
+                inkLvl = MAX;
+                cout << "Pen has been refilled\n";
+            }
+            else {
+                cout << "Cannot be refilled\n";
+            }
+        }
+};
+
+int main () {
+    MarkerPen pen ("Signature", "blue", true);
+
+    pen.write(60);
+    if (pen.showInkLvl() < 20) {
+        pen.refill();
+    }
+    pen.write(50);
+    cout << "Pen Ink Level is " << pen.showInkLvl() << endl;
+
+    MarkerPen pen2 ("Signature", "blue", false);
+    pen2.write(90);
+    if (pen2.showInkLvl() < 20) {
+        pen2.refill();
+    }
+}
