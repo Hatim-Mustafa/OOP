633ae5ed9c22e1fa5ec822d33aaf126dde19d0bd
Hatim-Mustafa <hatimmustafa108@gmail.com>
2025-02-05 19:43:37 +0000

    Task 4 done

---
+++ Lab3/task4.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+class Laptop {
+    public:
+        string brand;
+        string model;
+        int RAM;
+        int storage;
+        bool on;
+
+    public:
+        Laptop (string b, string m, int r, int s) {
+            brand = b;
+            model = m;
+            RAM = r;
+            storage = s;
+            on = false;
+        }
+
+        void turn_on () {
+            on = true;
+            cout << "Laptop turned on\n";
+        }
+
+        void turn_off () {
+            on = false;
+            cout << "Laptop turned off\n";
+        }
+
+        void run_program () {
+            if (on) {
+                cout << "Program is running\n"; 
+            }
+            else {
+                cout << "Cannot run program because laptop is turned off\n";
+            }
+        }
+
+        void specs () {
+            cout << "Brand name is " << brand << endl;
+            cout << "Model is " << model << endl;
+            cout << "RAM (in GB) is " << RAM << endl;
+            cout << "Storage (in GB) is " << storage << endl;
+        }
+
+        void compare(Laptop other) {
+            if (storage > other.storage) {
+                cout << "this user's laptop has more storage\n";
+            }
+            else {
+                cout << "the other user's laptop has more storage\n";
+            }
+
+            if (RAM > other.RAM) {
+                cout << "this user's laptop has more RAM\n";
+            }
+            else {
+                cout << "the other user's laptop has more RAM\n";
+            }
+        }
+};
+
+int main () {
+    Laptop BilalLaptop ("hp", "10rdf", 8, 256);
+    Laptop AyeshaLaptop ("dell", "Latitude7460", 16, 512);
+
+    BilalLaptop.compare(AyeshaLaptop);
+}
