95ff693622c616c1f03ea736566f0235ac4e7c3c
Hatim-Mustafa <hatimmustafa108@gmail.com>
2025-02-16 15:48:16 +0000

    Task 5 done

---
+++ Lab4/README.md
@@ -0,0 +1,16 @@
+<h2 align="center"> LAB 4 TASKS OUTPUT </h2>
+
+<h3> Task 1 </h3>
+<img src="images/image-4.png">
+
+<h3> Task 2 </h3>
+<img src="images/image-1.png">
+
+<h3> Task 3 </h3>
+<img src="images/image-2.png">
+
+<h3> Task 4 </h3>
+<img src="images/image-3.png">
+
+<h3> Task 5 </h3>
+<img src="images/image.png">
--- Lab4/task5.cpp
+++ Lab4/task5.cpp
@@ -4,27 +4,17 @@ using namespace std;
 
 class Book {
     public:
+    static int bookCount;
+    int id;
     string title;
     float price;
     int *stock;
 
-    Book() {
-        title = "Unknown";
-        price = 0.0;
-        stock = new int(30);
-    }
+    Book() : id(bookCount++), title("Unknown"), price(0.0), stock(new int(30)) {}
 
-    Book(string t, float p, int s) {
-        this->title = t;
-        this->price = p;
-        this->stock = new int(s);
-    }
+    Book(string t, float p, int s) : id(bookCount++), title(t), price(p), stock(new int(s)) {}
 
-    Book (Book& obj) {
-        this->title = obj.title;
-        this->price = obj.price;
-        this->stock = new int(*obj.stock);
-    }
+    Book (Book& obj) : id(bookCount++), title(obj.title), price(obj.price), stock(new int(*obj.stock)) {}
 
     ~Book () {
         delete stock;
@@ -74,10 +64,23 @@ class Book {
     float calculatePrice (int quantity, float discount) {
         return (float)quantity * discount * price;
     }
+
+    void displayDetails () {
+        cout << "BOOK DETAILS\n";
+        cout << "ID: " << this->id << endl;
+        cout << "Title: " << this->title << endl;
+        cout << "Price: " << this->price << endl;
+        cout << "Stock: " << *this->stock << endl;
+    }
 };
+
+int Book::bookCount = 0;
 
 int main(){
     Book b1("A Good Book", 12, 30);
-
+    b1.displayDetails();
     b1.purchase();
+    Book b2(b1);
+    b2.price = 15;
+    b2.displayDetails();
 }
